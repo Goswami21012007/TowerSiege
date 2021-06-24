17b762f4d1c965947a9e29abecd99b00bf9a3b2c
Goswami21012007 <82029506+Goswami21012007@users.noreply.github.com>
2021-06-24 20:37:46 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -1,5 +1,8 @@
 class Block {
     constructor(x,y,width,height) {
+      
+    
+  
       var options = {
         friction:1.0,
         density:0.04,
@@ -8,9 +11,17 @@ class Block {
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
+      this.Visiblity = 255;
+  
+  
       World.add(world, this.body);
     }
     display(){
+      
+
+
+
+
       push()
       var pos =this.body.position;
       rectMode(CENTER);
